79edeead79714d824b504abc7d46654657286583
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-22 02:28:45 +0900

    Update 20250922120000_create_lottery_entries.rb

--- db/migrate/20250922120000_create_lottery_entries.rb
+++ db/migrate/20250922120000_create_lottery_entries.rb
@@ -3,20 +3,20 @@
 class CreateLotteryEntries < ActiveRecord::Migration[7.0]
   def change
     create_table :lottery_entries do |t|
-      t.integer :topic_id, null: false
-      t.integer :user_id, null: false
+      t.bigint :topic_id, null: false
+      t.bigint :user_id, null: false
       t.string :title, null: false
       t.text :prize_description, null: false
-      t.integer :image_upload_id
+      t.bigint :image_upload_id
       t.datetime :draw_time, null: false
       t.integer :winner_count, null: false
-      t.string :specified_floors
+      t.text :specified_floors
       t.integer :min_participants, null: false
       t.string :backup_strategy, null: false
       t.text :additional_notes
       t.string :lottery_type, null: false
-      t.string :status, default: 'running', null: false
-      t.json :winners_data
+      t.string :status, null: false, default: 'running'
+      t.text :winners_data
       t.timestamps null: false
     end
 

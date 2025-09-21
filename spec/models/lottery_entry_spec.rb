c675246b26193657687a21e470a8779d4b604bbb
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-22 01:45:24 +0900

    Update lottery_entry_spec.rb

--- spec/models/lottery_entry_spec.rb
+++ spec/models/lottery_entry_spec.rb
@@ -117,4 +117,63 @@ RSpec.describe LotteryEntry, type: :model do
         draw_time: future_time
       )
       
-      allow(SiteSetting).to
+allow(SiteSetting).to receive(:lottery_post_lock_delay_minutes).and_return(30)
+      
+      expect(lottery.can_edit?).to be true
+      
+      # 测试接近锁定时间
+      near_lock_time = 15.minutes.from_now
+      lottery.draw_time = near_lock_time
+      expect(lottery.can_edit?).to be false
+    end
+  end
+  
+  describe '#time_until_draw' do
+    it 'calculates time remaining' do
+      future_time = 1.hour.from_now
+      lottery = LotteryEntry.new(draw_time: future_time)
+      
+      expect(lottery.time_until_draw).to be > 3500 # approximately 1 hour
+      expect(lottery.time_until_draw).to be < 3700
+    end
+    
+    it 'returns 0 for past time' do
+      past_time = 1.hour.ago
+      lottery = LotteryEntry.new(draw_time: past_time)
+      
+      expect(lottery.time_until_draw).to eq(0)
+    end
+  end
+  
+  describe '#participants_count' do
+    let(:lottery) { Fabricate(:lottery_entry, topic: topic, user: user) }
+    let(:participant1) { Fabricate(:user) }
+    let(:participant2) { Fabricate(:user) }
+    
+    before do
+      allow(SiteSetting).to receive(:lottery_excluded_groups).and_return('staff')
+    end
+    
+    it 'counts valid participants' do
+      # 创建参与回复
+      Fabricate(:post, topic: topic, user: participant1)
+      Fabricate(:post, topic: topic, user: participant2)
+      
+      expect(lottery.participants_count).to eq(2)
+    end
+    
+    it 'excludes lottery creator' do
+      Fabricate(:post, topic: topic, user: user) # 创建者的回复
+      Fabricate(:post, topic: topic, user: participant1)
+      
+      expect(lottery.participants_count).to eq(1)
+    end
+    
+    it 'excludes deleted posts' do
+      post = Fabricate(:post, topic: topic, user: participant1)
+      post.update!(deleted_at: Time.current)
+      
+      expect(lottery.participants_count).to eq(0)
+    end
+  end
+end
